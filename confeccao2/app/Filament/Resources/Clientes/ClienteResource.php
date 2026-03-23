95f437180fe22e9518d2bcf5f121a0ee8d091ea7
kayqu <kayquec158@gmail.com>
2026-03-23 16:51:54 -0300

    23.03.2026

--- confeccao2/app/Filament/Resources/Clientes/ClienteResource.php
+++ confeccao2/app/Filament/Resources/Clientes/ClienteResource.php
@@ -37,7 +37,8 @@ class ClienteResource extends Resource
                 textInput::make('email')->required()->email()->label('Email'),
                 textInput::make('telefone')->tel()->label('Telefone')->mask('(99) 99999-9999'),
                 textInput::make('documento')->label('CPF ou CNPJ')->mask(RawJs::make(<<<'JS'
-                $input.length > 14 ? '00.000.000/0000-00' : '000.000.000-00' JS)),
+                $input.length > 14 ? '00.000.000/0000-00' : '000.000.000-00' 
+                JS)),
 
                 Select::make('tipo')->label('Tipo')->options([
                     'fisica' => 'Pessoa Física',
